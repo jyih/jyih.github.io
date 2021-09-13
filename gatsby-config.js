f9c3e6bc8da22eb003623b99e89f88686b9a1ee6
jojo <johnathanyih@gmail.com>
2021-09-12 18:40:48 -0700

    updated projects, social, and work history

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,10 @@
 module.exports = {
+  siteMetadata: {
+    description: "Personal page of Johnathan Yih",
+    locale: "en",
+    title: "Johnathan Yih",
+    formspreeEndpoint: "https://formspree.io/f/myyloyyl",
+  },
   plugins: [
     {
       resolve: "@wkocjan/gatsby-theme-intro",
